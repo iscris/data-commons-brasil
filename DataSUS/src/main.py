44ae3be1f5a530d391981e52fdef2be2f86e364f
JonatasHdS <61765878+JonatasHdS@users.noreply.github.com>
2023-10-05 22:28:28 -0300

    DataSUS sem API

--- DataSUS/src/main.py
+++ DataSUS/src/main.py
@@ -3,57 +3,83 @@ from bs4 import BeautifulSoup as soup
 import re
 import os
 
-URL = "https://opendatasus.saude.gov.br/organization/ministerio-da-saude"
-PRE_URL = "https://opendatasus.saude.gov.br"
-initialPage = requests.get(URL)
-mediaPath = "../media/"
+def get_page_content(url):
+    response = requests.get(url)
+    return soup(response.content, "html.parser")
 
-content = soup(initialPage.content, "html.parser").find(id="content")
-pages = content.find_all("ul", class_="pagination")
-for page in pages:
-    links = page.find_all("a")
-    for link in links:
-        if link.text != "»":
-            print(f"Page {link.text}\n")         
-            link_url = link["href"]
-            page = requests.get(PRE_URL+link_url)
-            datasets = soup(page.content, "html.parser").find_all("li", class_="dataset-item")
-            for dataset in datasets:
-                heading = dataset.find("h3", class_="dataset-heading").find("a")                
-                print(heading.text)
-                if not os.path.exists(mediaPath+heading.text):
-                    os.makedirs(mediaPath+heading.text)
-                data = requests.get(PRE_URL+heading["href"])
-                resources = soup(data.content, "html.parser").find_all("li", class_="resource-item")
-                for resource in resources:
-                    resource_item = resource.find("a")
-                    print(resource_item.text)
-                    if resource_item.find("span").text == "PDF":                    
-                        data = requests.get(PRE_URL+resource_item["href"])
-                        item = soup(data.content, "html.parser").find("div", class_="btn-group").find("a") 
-                        
-                        response = requests.get(item["href"])
-                        pdf = open(mediaPath+heading.text+"/"+str(re.sub(r"[\n\t\s]*", "", resource_item.text))+".pdf", 'wb')
-                        pdf.write(response.content)
-                        pdf.close()
-                        
-                        print("Downloaded...")
-                    if resource_item.find("span").text == "CSV":
-                        data = requests.get(PRE_URL+resource_item["href"])
-                        item = soup(data.content, "html.parser").find("div", class_="btn-group")
-                        if item is not None:
-                            response = requests.get(item.find("a")["href"])
-                            csv = open(mediaPath+heading.text+"/"+str(re.sub(r"[\n\t\s]*", "", resource_item.text))+".csv", 'wb')
-                            csv.write(response.content)
-                            csv.close()
-                            
-                            print("Downloaded...")
-                        else:
-                            
-                print("\n")
+def create_directory(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-#datasets = content.find_all("li", class_="dataset-item")
-#for dataset in datasets:
-#    print(dataset, end="\n"*2)
+def download_resource(resource_url, save_path):
+    if not os.path.exists(save_path):
+        response = requests.get(resource_url)
+        with open(save_path, 'wb') as file:
+            file.write(response.content)
+        print("Downloaded...")
+    else:
+        print("Already Downloaded")
 
+def main():
+    URL = "https://opendatasus.saude.gov.br/organization/ministerio-da-saude"
+    PRE_URL = "https://opendatasus.saude.gov.br"
+    mediaPath = "../media/"
 
+    content = get_page_content(URL)
+    pages = content.find_all("ul", class_="pagination")
+
+    for page in pages:
+        links = page.find_all("a")
+        for link in links:
+            if link.text != "»":
+                print(f"Page {link.text}\n")
+                link_url = link["href"]
+                page = get_page_content(PRE_URL + link_url)
+                datasets = page.find_all("li", class_="dataset-item")
+
+                for dataset in datasets:
+                    heading = dataset.find("h3", class_="dataset-heading").find("a")
+                    print(heading.text)
+                    dataset_path = os.path.join(mediaPath, heading.text)
+                    create_directory(dataset_path)
+
+                    data = get_page_content(PRE_URL + heading["href"])
+                    resources = data.find_all("li", class_="resource-item")
+
+                    for resource in resources:
+                        resource_item = resource.find("a")
+                        print(resource_item.text)
+                        file_type = resource_item.find("span").text
+
+                        if file_type in ["PDF", "XLSX", "XLS", "ODT", "ZIP", "zip csv"]:
+                            data = get_page_content(PRE_URL + resource_item["href"])
+                            item = data.find("div", class_="btn-group").find("a")
+
+                            response = requests.get(item["href"])
+                            if file_type == "zip csv":
+                                file_name = re.sub(r"[\n\t\s]*", "", resource_item.text) + ".zip"
+                            else:
+                                file_name = re.sub(r"[\n\t\s]*", "", resource_item.text) + "." + file_type.lower()
+                            file_path = os.path.join(dataset_path, file_name)
+                            download_resource(item["href"], file_path)
+
+                        if file_type == "CSV":
+                            data = get_page_content(PRE_URL + resource_item["href"])
+                            item = data.find("div", class_="btn-group")
+                            if item is not None:
+                                response = requests.get(item.find("a")["href"])
+                                file_name = re.sub(r"[\n\t\s]*", "", resource_item.text) + "." + file_type.lower()
+                                file_path = os.path.join(dataset_path, file_name)
+                                download_resource(item.find("a")["href"], file_path)
+                            else:
+                                csvlist = data.find("div", class_="prose notes")
+                                for csvlink in csvlist.find_all("a", href=True):
+                                    csv_text = re.sub(r"[\n\t\s]*", "", csvlink.text)
+                                    file_name = csv_text + "." + file_type.lower()
+                                    file_path = os.path.join(dataset_path, file_name)
+                                    download_resource(csvlink['href'], file_path)
+
+                    print("\n")
+
+if __name__ == "__main__":
+    main()
