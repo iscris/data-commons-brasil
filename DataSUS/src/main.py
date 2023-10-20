c6543aface983a8bdc2210a2fe461ab7d0c4c578
JonatasHdS <61765878+JonatasHdS@users.noreply.github.com>
2023-10-20 01:36:52 -0300

    Att. DataSUS

--- DataSUS/src/main.py
+++ DataSUS/src/main.py
@@ -30,7 +30,11 @@ def clean_filename(filename):
 def main():
     URL = "https://opendatasus.saude.gov.br/organization/ministerio-da-saude"
     PRE_URL = "https://opendatasus.saude.gov.br"
+    PRE_URL_API = "https://apidadosabertos.saude.gov.br"
     mediaPath = "../media/"
+    usuario = None
+    senha = None
+
 
     content = get_page_content(URL)
     pages = content.find_all("ul", class_="pagination")
@@ -52,56 +56,79 @@ def main():
 
                     data = get_page_content(PRE_URL + heading["href"])
                     resources = data.find_all("li", class_="resource-item")
-
+        
                     for resource in resources:
                         resource_item = resource.find("a")
                         print(resource_item.text)
-                        file_type = resource_item.find("span").text
-
-                        if file_type in ["PDF", "XLSX", "XLS", "ODT", "ZIP", "zip csv", "API"]:
+                        file_type = resource_item.find("span").text.lower()
+                        if file_type in ["pdf", "xlsx", "xls", "odt", "zip", "zip csv", "api"]:
                             data = get_page_content(PRE_URL + resource_item["href"])
                             item = data.find("div", class_="btn-group").find("a")
 
-                            response = requests.get(item["href"])
                             if file_type == "zip csv":
                                 file_name = clean_filename(resource_item.text) + ".zip"
                                 file_path = os.path.join(dataset_path, file_name)                         
                                 download_resource(item["href"], file_path)
-                            if file_type == "API":  
+                            if file_type == "api":                                   
                                 paragraph_tags = data.find_all('p')
                                 for tag in paragraph_tags:
                                     text = tag.get_text()
                                     if "Usuário:" in text:
                                         usuario = text.split("Usuário:")[1].strip()
                                     elif "Senha:" in text:
-                                        senha = text.split("Senha:")[1].strip()
-                                        
-                                basic = HTTPBasicAuth(usuario,senha)
+                                        senha = text.split("Senha:")[1].strip()                                
+                                                                   
+                                
+                                if usuario is None or senha is None:
+                                    apidata = get_page_content(item["href"])
+                                    script_tags = apidata.find_all('script')
+                                    
+                                    user_config = None
+
+                                    for script_tag in script_tags:
+                                        javascript_code = script_tag.string if script_tag else None
+
+                                        if javascript_code and 'var user_config' in javascript_code:
+                                            lines = javascript_code.split('\n')
+
+                                            for line in lines:
+                                                if 'var user_config' in line:
+                                                    url_start = line.find('"url": "') + len('"url": "')
+                                                    url_end = line.find('"', url_start)
+                                                    url = line[url_start:url_end]
+
+                                    item["href"] = PRE_URL_API + url
+                                    basic = ""
+                                else:
+                                    basic = HTTPBasicAuth(usuario,senha)
                                 
                                 file_name = clean_filename(resource_item.text) + ".json"
                                 file_path = os.path.join(dataset_path, file_name)                         
                                 download_resource(item["href"], file_path, basic)
+                                usuario = None
+                                senha = None
                             else:
-                                file_name = clean_filename(resource_item.text) + "." + file_type.lower()
+                                file_name = clean_filename(resource_item.text) + "." + file_type
                                 file_path = os.path.join(dataset_path, file_name)                         
                                 download_resource(item["href"], file_path)
                                                       
-                        if file_type == "CSV":
+                        if file_type == "csv":
                             data = get_page_content(PRE_URL + resource_item["href"])
                             item = data.find("div", class_="btn-group")
                             if item is not None:
                                 response = requests.get(item.find("a")["href"])
-                                file_name = clean_filename(resource_item.text) + "." + file_type.lower()
+                                file_name = clean_filename(resource_item.text) + "." + file_type
                                 file_path = os.path.join(dataset_path, file_name)
                                 download_resource(item.find("a")["href"], file_path)
                             else:
                                 csvlist = data.find("div", class_="prose notes")
                                 for csvlink in csvlist.find_all("a", href=True):
                                     csv_text = clean_filename(csvlink.text)
-                                    file_name = csv_text + "." + file_type.lower()
+                                    file_name = csv_text + "." + file_type
                                     file_path = os.path.join(dataset_path, file_name)
                                     download_resource(csvlink['href'], file_path)
                     print("\n")
 
 if __name__ == "__main__":
     main()
+
