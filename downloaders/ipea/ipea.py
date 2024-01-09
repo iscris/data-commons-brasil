d6d623dbeb989b71e1e3669ff4def2ea8c01d33f
Isadora <isadoracmr4@gmail.com>
2024-01-09 15:08:40 -0300

    chore: comment out code to test

--- downloaders/ipea/ipea.py
+++ downloaders/ipea/ipea.py
@@ -5,6 +5,7 @@ import ipeadatapy
 
 
 def get_values(code: str):
+    print(f"Downloading metadata for dataset {code}")
     return ipeadatapy.api_call(
         f"http://www.ipeadata.gov.br/api/odata4/Metadados('{code}')/Valores"
     )
@@ -18,12 +19,13 @@ ipeadatapy.sources().to_csv("downloads/sources.csv", index=False)
 ipeadatapy.themes().to_csv("downloads/themes.csv", index=False)
 ipeadatapy.territories().to_csv("downloads/territories.csv", index=False)
 
-df = ipeadatapy.list_series()
-code_list = df["CODE"].tolist()
-
-with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
-    results = executor.map(get_values, code_list)
-    for result in results:
-        code = result.iloc[0]["SERCODIGO"]
-        os.makedirs("downloads/values", exist_ok=True)
-        result.to_csv(f"downloads/values/{code}.csv")
+# df = ipeadatapy.list_series()
+# code_list = df["CODE"].tolist()
+# 
+# with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+#     results = executor.map(get_values, code_list)
+#     for result in results:
+#         code = result.iloc[0]["SERCODIGO"]
+#         os.makedirs("downloads/values", exist_ok=True)
+#         result.to_csv(f"downloads/values/{code}.csv")
+# 
