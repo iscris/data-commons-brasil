72320e2d15a715de5c3c285d107950df19faa533
Isadora <isadoracmr4@gmail.com>
2024-10-31 17:31:43 -0300

    chore: fix processor output path

--- datatools/processor/processor.py
+++ datatools/processor/processor.py
@@ -9,7 +9,7 @@ def pick_processor(source: str, input_path: str, tmp_output_path: str):
     match source:
         case "ipea":
             process_ipea_data(
-                input_path, f"{tmp_output_path}/{source}", f"{input_path}/values"
+                input_path, tmp_output_path, f"{input_path}/values"
             )
         case _:
             raise NotImplementedError
@@ -21,14 +21,16 @@ def process_source(source: str, input_path: str, output_path: str):
     process_id = uuid4()
     # A temporary path is created to make sure that errors in processing
     # won't result in losing the last valid output
-    tmp_path = f"{output_path}/{source}/tmp_{process_id}"
+    tmp_path = f"{output_path}/tmp_{process_id}"
+    final_path = f"{output_path}/{source}"
     pick_processor(source, input_path, tmp_path)
 
-    os.rmdir(output_path)
-    os.makedirs(output_path, exist_ok=True)
-    move_content(tmp_path, output_path)
+    shutil.rmtree(final_path)
+    os.makedirs(final_path, exist_ok=True)
+    move_content(tmp_path, final_path)
+    print("cheguei aqui 2")
     os.rmdir(tmp_path)
-    print(f"Download concluded ({output_path})")
+    print(f"Download concluded ({final_path})")
 
 
 def move_content(source_dir: str, destination: str):
