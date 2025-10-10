c08eec8f09eacc28e27dab8cfca3f18d5541687f
iscris <isadoracmr4@gmail.com>
2025-10-10 17:40:32 -0300

    chore: update ibge agregados downloader

--- datatools/downloaders/ibge/agregados.py
+++ datatools/downloaders/ibge/agregados.py
@@ -1,78 +1,103 @@
-import requests
 import json
 import logging
+import os
+import time
 
-from datatools.downloaders.utils import rate_limit, retry
+import requests
 
 logger = logging.getLogger("downloader: IBGE agregados")
+logging.basicConfig(level=logging.DEBUG)
 
-# N1 - Brasil; N3 - Unidade da Federação; N6 - Município
 DC_PLACES = ["N1", "N3", "N6"]
 MAX_CALLS_PER_INTERVAL = 120
 INTERVAL = 60
 
 
-@retry(max_attempts=10, delay=2*60, backoff_factor=3)
 def make_api_call(url: str):
-    logger.debug(f"Making API call: {url}")
-    r = requests.get(url)
+    retries = 0
+    max_retries = 5
+    while retries < max_retries:
+        try:
+            logger.debug(f"Making API call: {url} {retries=}")
+            r = requests.get(url, timeout=120)
+            if r.status_code == 200:
+                logger.debug("Request concluded")
+                return r.json()
+        except requests.exceptions.Timeout:
+            logger.error("Request timeout")
+        except Exception as e:
+            logger.error(f"Request failed: Status {e}")
 
-    if r.status_code != 200:
-        logger.error(f"Falha na requisição: Status {r.status_code}")
-        logger.debug(f"Response: {r.content}")
-        return {}
+        retries += 1
+        logger.debug(f"Retrying after sleep time: {retries=}")
+        time.sleep(120)
 
-    return r.json()
-
-
-def get_aggregates() -> dict:
-    return make_api_call("https://servicodados.ibge.gov.br/api/v3/agregados")
+    return {}
 
 
-@rate_limit(calls=120, period=30)
+def get_aggregates() -> list[str]:
+    aggregates_json = make_api_call("https://servicodados.ibge.gov.br/api/v3/agregados")
+    return [
+        aggregate["id"]
+        for item in aggregates_json
+        for aggregate in item.get("agregados", [])
+    ]
+
+
 def get_aggregates_metadata(aggregate_id: str) -> dict:
     return make_api_call(
         f"https://servicodados.ibge.gov.br/api/v3/agregados/{aggregate_id}/metadados"
     )
 
 
-@rate_limit(calls=120, period=60)
 def get_values(aggregate_id: str, variable_id: str, place_id: str):
     return make_api_call(
         f"https://servicodados.ibge.gov.br/api/v3/agregados/{aggregate_id}/periodos/-100/variaveis/{variable_id}?localidades={place_id}"
     )
 
 
-def download_ibge_agregados(output_dir: str) -> dict[str, dict[str, str]]:
-    def process_aggregate_metadata(
-        aggregate: dict[str, str | dict[str, list[str]]],
-    ) -> dict[str, str]:
-        variables_ids = [item["id"] for item in aggregate.get("variaveis")]
+def process_and_save_aggregate(aggregate_id: str, output_dir: str):
+    logger.info(f"Processing aggregate ID: {aggregate_id}")
 
-        defined_places: list[str] = aggregate["nivelTerritorial"]["Administrativo"]
-        places_to_search = [place for place in defined_places if place in DC_PLACES]
-        series = []
-        for variable_id in variables_ids:
-            for place in places_to_search:
-                values = get_values(aggregate["id"], variable_id, place)
+    output_path = os.path.join(output_dir, f"{aggregate_id}.json")
+    if os.path.exists(output_path):
+        logger.info(f"Aggregate {aggregate_id} already exists. Skipping.")
+        return
+
+    metadata = get_aggregates_metadata(aggregate_id)
+    if not metadata:
+        logger.warning(f"Empty metadata for aggregate {aggregate_id}")
+        return
+
+    variables_ids = [item["id"] for item in metadata.get("variaveis", [])]
+    defined_places: list[str] = metadata.get("nivelTerritorial", {}).get(
+        "Administrativo", []
+    )
+    places_to_search = [place for place in defined_places if place in DC_PLACES]
+
+    series = []
+    for variable_id in variables_ids:
+        for place in places_to_search:
+            values = get_values(aggregate_id, variable_id, place)
+            if values:
                 series.append(values)
 
-        return {"assunto": aggregate.get("assunto", ""), "values": series}
-
-    aggregates_json = get_aggregates()
-    aggregates_ids = [
-        aggregate["id"]
-        for item in aggregates_json
-        for aggregate in item.get("agregados", [])
-    ]
-
-    aggregates_metadata = {id: get_aggregates_metadata(id) for id in aggregates_ids}
-
-    aggregates_cleaned = {
-        id: process_aggregate_metadata(aggregate)
-        for id, aggregate in aggregates_metadata.items()
+    result = {
+        "assunto": metadata.get("assunto", ""),
+        "values": series,
     }
 
-    for aggregate_id, metadata in aggregates_cleaned.items():
-        with open(f"{output_dir}/{aggregate_id}.json", "w+", encoding="utf-8") as file:
-            json.dump(metadata, file, ensure_ascii=False)
+    with open(output_path, "w+", encoding="utf-8") as file:
+        json.dump(result, file, ensure_ascii=False)
+    logger.info(f"Saved aggregate {aggregate_id} to {output_path}")
+
+
+def download_ibge_agregados(output_dir: str, skip_files: list[str]):
+    os.makedirs(output_dir, exist_ok=True)
+    aggregate_ids = get_aggregates()
+    for aggregate_id in aggregate_ids:
+        if f"{aggregate_id}.json" in skip_files:
+            logger.info(f"File already exists: {aggregate_id=}.")
+            continue
+
+        process_and_save_aggregate(aggregate_id, output_dir)
